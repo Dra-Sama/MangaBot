1ccf28b0a67004b60e95fa7f42908127798e6e1e
Daniel Rivero <danielrivgw@gmail.com>
2022-03-07 23:45:23 -0500

    Real TMO

--- plugins/tmo.py
+++ plugins/tmo.py
@@ -59,7 +59,7 @@ class TMOClient(MangaClient):
 
         images = ul.find_all('img')
 
-        images_url = [quote(img.get('data-src'), safe=':/') for img in images]
+        images_url = [quote(img.get('src'), safe=':/') for img in images]
 
         return images_url
 
