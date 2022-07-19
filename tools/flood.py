8314e8e4cbd34a856172065eda5871dac57d26b5
Daniel Rivero <danielrivgw@gmail.com>
2022-07-19 14:09:55 -0400

    Fix flood handling

--- tools/flood.py
+++ tools/flood.py
@@ -1,22 +1,25 @@
 import asyncio
+from typing import Callable, Awaitable, Any
 
 import pyrogram.errors
 
 
 # retries an async awaitable as long as it raises FloodWait, and waits for err.x time
-async def retry_on_flood(awaitable):
-    while True:
-        try:
-            return await awaitable
-        except pyrogram.errors.FloodWait as err:
-            print(f'FloodWait, waiting {err.x} seconds')
-            await asyncio.sleep(err.x)
-            continue
-        except pyrogram.errors.RPCError as err:
-            if err.MESSAGE == 'FloodWait':
+def retry_on_flood(function: Callable[[Any], Awaitable]):
+    async def wrapper(*args, **kwargs):
+        while True:
+            try:
+                return await function(*args, **kwargs)
+            except pyrogram.errors.FloodWait as err:
+                print(f'FloodWait, waiting {err.x} seconds')
                 await asyncio.sleep(err.x)
                 continue
-            else:
+            except pyrogram.errors.RPCError as err:
+                if err.MESSAGE == 'FloodWait':
+                    await asyncio.sleep(err.x)
+                    continue
+                else:
+                    raise err
+            except Exception as err:
                 raise err
-        except Exception as err:
-            raise err
+    return wrapper
