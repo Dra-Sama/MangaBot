762805837cc06dbd4e0a4bfe24c19bcc39f64fa3
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-04-07 19:00:16 +0545

    Update main.py

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     loop = aio.get_event_loop_policy().get_event_loop()
     loop.run_until_complete(async_main())
     loop.create_task(manga_updater())
+    for i in range(10):
+        loop.create_task(chapter_creation(i + 1))
     bot.run()
-
