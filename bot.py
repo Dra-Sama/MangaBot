b30ba73579318b36e5cadd0ae525ade9cdf50594
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-04-12 16:56:47 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ plugin_dicts: Dict[str, Dict[str, MangaClient]] = {
         "MangaBuddy": MangaBuddyClient(),
         "AsuraScans": AsuraScansClient(),
         "NineManga": NineMangaClient(),        
-        "Comick": ComickClient(),
+        "Comick": ComickClient(), 
         "Likemanga": LikemangaClient(),
           },
     "🇪🇸 ES": {
