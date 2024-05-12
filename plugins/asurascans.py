6181bcefa778bd5eae8eb220e9b3c2375b140599
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-05-12 20:28:38 +0545

    Update asurascans.py

--- plugins/asurascans.py
+++ plugins/asurascans.py
@@ -11,7 +11,7 @@ from plugins.client import MangaClient, MangaCard, MangaChapter, LastChapter
 
 class AsuraScansClient(MangaClient):
 
-    base_url = urlparse("https://www.asurascans.com/")
+    base_url = urlparse("https://www.asura-scans.com/")
     search_url = base_url.geturl()
     search_param = 's'
     updates_url = base_url.geturl()
