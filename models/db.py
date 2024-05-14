6891352f307c66950834ed45c8d80ec8176b45e6
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-05-14 21:26:26 +0545

    Update db.py

--- models/db.py
+++ models/db.py
@@ -9,6 +9,27 @@ from tools import LanguageSingleton
 
 T = TypeVar("T")
 
+user_data = database['users']
+
+async def present_user(user_id : int):
+    found = user_data.find_one({'_id': user_id})
+    return bool(found)
+
+async def add_user(user_id: int):
+    user_data.insert_one({'_id': user_id})
+    return
+
+async def full_userbase():
+    user_docs = user_data.find()
+    user_ids = []
+    for doc in user_docs:
+        user_ids.append(doc['_id'])
+        
+    return user_ids
+
+async def del_user(user_id: int):
+    user_data.delete_one({'_id': user_id})
+    return
 
 class ChapterFile(SQLModel, table=True):
     url: str = Field(primary_key=True)
