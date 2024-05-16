551d78414e7b74b8f48c9eba6ec4307f9e1b034e
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-05-16 09:20:43 +0545

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from bot import bot, manga_updater, chapter_creation
 from models import DB
 
 
-PIC = "https://te.legra.ph/file/5a2a761e6a899163db6d4.jpg"
-
+PIC = "https://graph.org//file/925c5eee60879804be1d9.jpg"
 async def async_main():
     db = DB()
     await db.connect()
@@ -16,7 +15,6 @@ if __name__ == '__main__':
     loop = aio.get_event_loop_policy().get_event_loop()
     loop.run_until_complete(async_main())
     loop.create_task(manga_updater())
-    bot.start()
-    bot.send_photo(-1001723894782, photo=PIC, caption="Hey Guys! I'm alive.")
-    idle()
-    bot.stop()
+    for i in range(10):
+        loop.create_task(chapter_creation(i + 1))
+    bot.run()
