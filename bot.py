be1c5a5917686708b04a0b5b5e452a0e985979ee
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-09-04 13:32:05 +0545

    Update bot.py
    Fixing Updates

--- bot.py
+++ bot.py
@@ -76,7 +76,6 @@ with open("tools/help_message.txt", "r") as f:
 class OutputOptions(enum.IntEnum):
     PDF = 1
     CBZ = 2
-    Telegraph = 4
 
     def __and__(self, other):
         return self.value & other
