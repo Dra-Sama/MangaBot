7790cb5b9473fd439ccfc2c677166538324e9f75
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-10-02 13:48:26 +0545

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3,7 +3,7 @@ from .manhuako import ManhuaKoClient
 from .tmo import TMOClient
 from .mangadex import MangaDexClient
 from .mangasin import MangasInClient
-from .mcreader import McReaderClient
+from .mcreader import MgekoClient
 from .mangakakalot import MangaKakalotClient
 from .manganelo import ManganeloClient
 from .manganato import ManganatoClient
@@ -15,3 +15,4 @@ from .asurascans import AsuraScansClient
 from .ninemanga import NineMangaClient
 from .likemanga import LikeMangaClient
 from .mangasee import MangaSeeClient
+from .mangadistrict import MangaDistrictClient
