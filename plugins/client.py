4027cc90700d65a4dd2e59557e7feb32d227a4f8
justatest420 <justatestsubject.01@gmail.com>
2025-03-18 08:39:18 +0545

    Update client.py

--- plugins/client.py
+++ plugins/client.py
@@ -3,7 +3,7 @@ from abc import abstractmethod, ABC
 from dataclasses import dataclass
 from typing import List, AsyncIterable
 
-from aiohttp import ClientSession
+from httpx import AsyncClient
 from pathlib import Path
 
 from models import LastChapter
@@ -48,6 +48,118 @@ def clean(name, length=-1):
     return name
 
 
+
+class MangaClient(AsyncClient, metaclass=LanguageSingleton):
+
+    def __init__(self, *args, name="client", **kwargs):
+        if name == "client":
+            raise NotImplementedError
+        super().__init__(*args, **kwargs)
+        self.name = name
+
+    async def get_url(self, url, *args, file_name=None, cache=False, req_content=True, method='get', data=None,
+                      **kwargs):
+        def response():
+            pass
+
+        response.status_code = 200  # httpx uses status_code instead of status
+        if cache:
+            path = Path(f'cache/{self.name}/{file_name}')
+            os.makedirs(path.parent, exist_ok=True)
+            try:
+                with open(path, 'rb') as f:
+                    content = f.read()
+            except FileNotFoundError:
+                if method == 'get':
+                    response = await self.get(url, *args, **kwargs)
+                elif method == 'post':
+                    response = await self.post(url, data=data or {}, **kwargs)
+                else:
+                    raise ValueError
+                if str(response.status_code).startswith('2'):
+                    content = response.content  # httpx uses .content instead of .read()
+                    with open(path, 'wb') as f:
+                        f.write(content)
+        else:
+            if method == 'get':
+                response = await self.get(url, *args, **kwargs)
+            elif method == 'post':
+                response = await self.post(url, data=data or {}, **kwargs)
+            else:
+                raise ValueError
+            content = response.content  # httpx uses .content instead of .read()
+        if req_content:
+            return content
+        else:
+            return response
+
+    async def set_pictures(self, manga_chapter: MangaChapter):
+        requests_url = manga_chapter.url
+
+        # Set manga url as the referer if there is one
+        headers = {**self.headers}
+        if manga_chapter.manga:
+            headers['referer'] = manga_chapter.manga.url
+
+        response = await self.get(requests_url, headers=headers)
+
+        content = response.content  # httpx uses .content instead of .read()
+
+        manga_chapter.pictures = await self.pictures_from_chapters(content, response)
+
+        return manga_chapter
+
+    async def download_pictures(self, manga_chapter: MangaChapter):
+        if not manga_chapter.pictures:
+            await self.set_pictures(manga_chapter)
+
+        folder_name = f'{clean(manga_chapter.manga.name)}/{clean(manga_chapter.name)}'
+        i = 0
+        for picture in manga_chapter.pictures:
+            ext = picture.split('.')[-1].split('?')[0].lower()
+            file_name = f'{folder_name}/{format(i, "05d")}.{ext}'
+            for _ in range(3):
+                req = await self.get_picture(manga_chapter, picture, file_name=file_name, cache=True,
+                                             req_content=False)
+                if str(req.status_code).startswith('2'):  # httpx uses status_code instead of status
+                    break
+            else:
+                raise ValueError
+            i += 1
+
+        return Path(f'cache/{manga_chapter.client.name}') / folder_name
+
+    async def get_picture(self, manga_chapter: MangaChapter, url, *args, **kwargs):
+        return await self.get_url(url, *args, **kwargs)
+
+    async def get_cover(self, manga_card: MangaCard, *args, **kwargs):
+        return await self.get_url(manga_card.picture_url, *args, **kwargs)
+
+    async def check_updated_urls(self, last_chapters: List[LastChapter]):
+        return [lc.url for lc in last_chapters], []
+
+    @abstractmethod
+    async def search(self, query: str = "", page: int = 1) -> List[MangaCard]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_chapters(self, manga_card: MangaCard, page: int = 1) -> List[MangaChapter]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def contains_url(self, url: str):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def iter_chapters(self, manga_url: str, manga_name: str) -> AsyncIterable[MangaChapter]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def pictures_from_chapters(self, content: bytes, response=None):
+        raise NotImplementedError
+
+
+"""
 class MangaClient(ClientSession, metaclass=LanguageSingleton):
 
     def __init__(self, *args, name="client", **kwargs):
@@ -156,3 +268,4 @@ class MangaClient(ClientSession, metaclass=LanguageSingleton):
     @abstractmethod
     async def pictures_from_chapters(self, content: bytes, response=None):
         raise NotImplementedError
+"""
